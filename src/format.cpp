51a1c596df85a6f9dcef314b663117aeff1855c6
christhian.garcia <christhian.garcia.b@gmail.com>
2022-09-15 23:45:41 -0700

    remove warnings

--- src/format.cpp
+++ src/format.cpp
@@ -11,8 +11,18 @@ string Format::ElapsedTime(long seconds) {
   seconds -= hours * 3600;
   int minutes = seconds / 60;
   seconds -= minutes * 60;
-  struct tm format_time = {
-      .tm_sec = seconds, .tm_min = minutes, .tm_hour = hours};
+  struct tm format_time = {.tm_sec = (int)seconds,
+                           .tm_min = minutes,
+                           .tm_hour = hours,
+                           // just to remove warning
+                           .tm_mday = 1,
+                           .tm_mon = 0,
+                           .tm_year = 2022,
+                           .tm_wday = 0,
+                           .tm_yday = 0,
+                           .tm_isdst = 0,
+                           .tm_gmtoff = 0,
+                           .tm_zone = 0};
   char buffer[10];
   strftime(buffer, sizeof buffer, "%T", &format_time);
   return string(buffer);
